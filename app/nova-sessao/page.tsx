03a98e1a3e9e2f17b233c3664f0de279f126128e
Pedro F <pedrospf2018@gmail.com>
2026-02-25 16:57:51 -0300

    feat: added Sadhcn Components

---
+++ app/nova-sessao/page.tsx
@@ -0,0 +1,9 @@
+import { StudySessionForm } from "./components/StudySessionForm";
+
+export default function NovaSessaoPage() {
+    return (
+        <div>
+            <StudySessionForm />
+        </div>
+    );
+}
